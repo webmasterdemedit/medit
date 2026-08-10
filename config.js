356304bc478eb9457b0cf529b12579511db23999
webmasterdemedit <was16e@gmail.com>
2026-08-10 20:51:21 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,12 @@
+// ============================================================
+// CONFIGURATION CENTRALISÉE
+// ============================================================
+
+var CONFIG = {
+  // L'URL de ton script Google Apps
+  SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbzSpGKSvmEMhcYByzGAHta-9ZQvE3GkJlExfmR295PG6LLlF3u339moDFEP2Y26kJs7/exec',
+  
+  // Autres paramètres éventuels
+  SITE_NAME: 'Méditations islamiques',
+  SITE_URL: 'https://webmasterdemedit.github.io/medit/'
+};
