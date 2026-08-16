25be3e0ee3b719e78fdefaea561b88bfc87ba43e
meditislam <was16e@gmail.com>
2026-08-16 10:28:57 +0200

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@
 
 var CONFIG = {
   // L'URL de ton script Google Apps (À METTRE À JOUR APRÈS DÉPLOIEMENT)
-  SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbw6XjSFi8yOXtD4kSGoHX5bEDCrPbIcCOa6ZwTnVt4LHMO1nh-g9OZ4uBIlyZ_F71c4/exec',
+  SCRIPT_URL: 'https://script.google.com/macros/s/AKfycby3VuLLj13USe8iOeTJ1TNSIT-akPpqeFWP5tOykqI7O5ouPDKWrPR7d5JwlX7jWrAH/exec',
   
   SITE_NAME: 'Méditations islamiques',
   SITE_URL: 'https://webmasterdemedit.github.io/medit/'
